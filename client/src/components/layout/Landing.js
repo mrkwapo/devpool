a7d1f919f12a0959b9dce0eed924dfa18571a5fb
mrkwapo <mrkwapo@gmail.com>
2019-07-07 01:39:04 -0400

    set up React and components

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <section className='landing'>
+      <div className='dark-overlay'>
+        <div className='landing-inner'>
+          <h1 className='x-large'>Developer Pool</h1>
+          <p className='lead'>
+            Create a developer profile/resume, share posts and get hired by
+            recruiters.
+          </p>
+          <div className='buttons'>
+            <a href='register.html' className='btn btn-primary'>
+              Sign Up
+            </a>
+            <a href='login.html' className='btn btn-light'>
+              Login
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Landing;
