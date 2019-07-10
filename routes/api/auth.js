302242728571699625c07446a517d6e559750ad0
mrkwapo <mrkwapo@gmail.com>
2019-07-10 00:10:45 -0400

    Protected Route For Dashboard

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const auth = require('../../middleware/auth');
-const User = require('../../models/User');
 const jwt = require('jsonwebtoken');
 const config = require('config');
 const { check, validationResult } = require('express-validator');
+
+const User = require('../../models/User');
 
 // @route      GET api/auth
 // @desc       Test route
